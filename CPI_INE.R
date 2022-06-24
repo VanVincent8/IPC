e027364a9df2f3378714c8ad23233e3af7def939
Vicent Marí Roselló <Vicent@MacBook-Air-de-Vicent.local>
2022-06-24 18:28:57 +0200

    24062022 - v.1.2 - Versió final

--- CPI_INE.R
+++ CPI_INE.R
@@ -1,31 +1,22 @@
-if (!require(dplyr)) install.packages("dplyr"); library(dplyr)
-if (!require(httr)) install.packages("httr"); library(httr)
-if (!require(jsonlite)) install.packages("jsonlite"); library(jsonlite)
-if (!require(rjstat)) install.packages("rjstat"); library(rjstat)
-if (!require(tictoc)) install.packages("tictoc"); library(tictoc)
-if (!require(highcharter)) install.packages("highcharter"); library(highcharter)
-library(stringr)
+#########################################################
+#                                                       #
+#        SCRIPT TO DOWNLOAD DISSAGREGATED CPI           #
+#     FROM MADRID, BARCELONA, CATALONIA AND SPAIN       #
+#                                                       #
+#########################################################
+
+###-----------------------------------------------------------------------------
+# Libraries
+library(httr)
+library(jsonlite)
+library(tictoc)
 library(tidyverse)
 library(lubridate)
 library(rlist)
-library(magrittr)
+library(vctrs)
 
-rm(list = ls()) 
-#################################################
-
-# Per tal d'extreure dades mitjançant el creuament de metadades s'ha d'utilitzar un altre tipus d'url.
-# Amb el DATOS_SERIE podíem extreure les dades individualment, però amb DATOS_METADATAOPERACIÓN podem 
-# afegir els paràmetres de búsqueda.
-
-# Per exemple, podem fer una petició tal com "/DATOS_METADAOPERACIÓN/IPC?g1=115:29&g2=3:74&g3=762:&nult=1"
-# Després de l'IPC no especificam cap codi, això es farà en els paràmetres:
-
-# g1=115:29, 115 és el codi per la selecció de províncies i 29 és el codi de Madrid. A código_provincias es pot buscar el codi de qualsevol
-# g2=3:74, 3 és el codi per al grup de TIPO DE DATOS i 74 es refereix a la variació anual. A códigos_tipo_dato es pot veure els diferents codis per cada mesura
-# g3=762, fa referència als grups ECOICOP (els del nostre interès) i els seleccionam tots.
-# nult=1, en aquest tipus de url no es pot posar un rang de dates sinó les últimes dates disponibles, llavors nult=1 retornarà l'última data disponible
-
-
+###-----------------------------------------------------------------------------
+# Some useful tables
 categorias_url <- "https://servicios.ine.es/wstempus/js/ES/VARIABLES_OPERACION/IPC"
 res_categorias_url <- GET(categorias_url)
 codigos_categorias <- fromJSON(rawToChar(res_categorias_url$content))
@@ -35,8 +26,8 @@ res_tipo_dato_url <- GET(tipo_dato_url)
 codigos_tipo_dato <- fromJSON(rawToChar(res_tipo_dato_url$content))
 
 provincias_url <- "https://servicios.ine.es/wstempus/js/ES/VALORES_VARIABLEOPERACION/115/IPC"
-provincias_url <- GET(provincias_url)
-codigo_provincias <- fromJSON(rawToChar(provincias_url$content))
+res_provincias_url <- GET(provincias_url)
+codigo_provincias <- fromJSON(rawToChar(res_provincias_url$content))
 
 comunidades_url <- "https://servicios.ine.es/wstempus/js/ES/VALORES_VARIABLEOPERACION/70/IPC"
 res_comunidades_url <- GET(comunidades_url)
@@ -54,10 +45,28 @@ grupos_ECOICOP_url <- "https://servicios.ine.es/wstempus/js/ES/VALORES_VARIABLEO
 res_grupos_ECOICOP_url <- GET(grupos_ECOICOP_url)
 codigos_grupos_ECOICOP <- fromJSON(rawToChar(res_grupos_ECOICOP_url$content))
 
-#################################################
-# INE 
-# Madrid
-
+###-----------------------------------------------------------------------------
+# Functions
+extract_CPI <- function(data, x) {
+  data_frame  <- data %>%
+    list.extract(x) %>%
+    mutate(Regio = categories[[1]][x],
+           Categoria = categories[[2]][x],
+           Data = as.POSIXct(Fecha/1000, origin="1970-01-01")) %>%
+    select(Categoria, Data, Valor, Regio)
+  data_frame
+}
+extract_core_CPI <- function(data, x) {
+  data_frame  <- data %>%
+    list.extract(x) %>%
+    mutate(Regio = categories[[1]][x],
+           Categoria = categories[[2]][x],
+           Data = as.POSIXct(Fecha/1000, origin="1970-01-01")) %>%
+    select(Data, Valor)
+  data_frame
+}
+###-----------------------------------------------------------------------------
+### Madrid ###
 tic()
 period <- days(today() - ymd(20000101))$day
 url <- str_c("https://servicios.ine.es/wstempus/js/ES/DATOS_METADATAOPERACION/IPC?g1=70:9009&g2=3:74&g3=762:&p=1&nult=",period)
@@ -69,33 +78,18 @@ categories <- fromJSON(content(res, "text", encoding = "UTF-8")) %>%
   as_tibble() %>%
   rename(Nombre = ".") %>%
   tidyr::separate(Nombre, into = c("Regio", "Categoria"), sep = "\\. ")
+raw_data <- fromJSON(rawToChar(res$content))$Data
 
-raw_data <- fromJSON(rawToChar(res$content))$Data 
-
-# Extreim les llistes
-my_function <- function(data, x) {
-  data_frame  <- data %>%
-    list.extract(x) %>%
-    mutate(Regio = categories[[1]][x],
-           Categoria = categories[[2]][x],
-           Data = as.POSIXct(Fecha/1000, origin="1970-01-01")) %>%
-    select(Categoria, Data, Valor, Regio)
-  data_frame
-}
-
-# Cream un data frame buit i anam copiant les llistes a la data frame
-IPC_INE_MAD <- vctrs::data_frame(.size = 0)
+# Create an empty data frame i copy the lists into the dataframe
+CPI_INE_MAD <- vctrs::data_frame(.size = 0)
 for (i in 1:13) {
-  data <- as_tibble(my_function(raw_data, i)) 
-  IPC_INE_MAD <- rbind(data, IPC_INE_MAD)
+  data <- as_tibble(extract_CPI(raw_data, i))
+  CPI_INE_MAD <- rbind(data, CPI_INE_MAD)
 }
-
-IPC_INE_MAD <- IPC_INE_MAD %>% arrange(Data) %>% filter(Data > ymd(20020101)) %>% 
+CPI_INE_MAD <- CPI_INE_MAD %>% arrange(Data) %>% filter(Data > ymd(20020101)) %>%
   spread(key = "Categoria", value = "Valor")
 
-######
-
-# IPC subjacent
+### Core CPI ###
 period <- days(today() - ymd(20000101))$day
 url <- str_c("https://servicios.ine.es/wstempus/js/ES/DATOS_METADATAOPERACION/IPC?g1=70:9009&g2=3:74&g3=269:12850&p=1&nult=",period)
 res =  GET(url)
@@ -107,39 +101,18 @@ categories <- fromJSON(content(res, "text", encoding = "UTF-8")) %>%
   as_tibble() %>%
   rename(Nombre = ".") %>%
   tidyr::separate(Nombre, into = c("Regio", "Categoria"), sep = "\\. ")
+raw_data <- fromJSON(rawToChar(res$content))$Data[2]
 
-raw_data <- fromJSON(rawToChar(res$content))$Data[2] 
+# Create an empty data frame i copy the lists into the dataframe
+CPI_INE_MAD_sub <- vctrs::data_frame(.size = 0)
+data <- as_tibble(extract_core_CPI(raw_data, 1))
+CPI_INE_MAD_sub <- rbind(data, CPI_INE_MAD_sub)  %>% arrange(Data) %>% filter(Data > ymd(20020101))
+CPI_INE_MAD <- CPI_INE_MAD %>%
+  cbind(CPI_INE_MAD_sub$Valor) %>%
+  rename(Core = "CPI_INE_MAD_sub$Valor")
 
-# Extreim les llistes
-my_function <- function(data, x) {
-  data_frame  <- data %>%
-    list.extract(x) %>%
-    mutate(Regio = categories[[1]][x],
-           Categoria = categories[[2]][x],
-           Data = as.POSIXct(Fecha/1000, origin="1970-01-01")) %>%
-    select(Data, Valor) 
-  data_frame
-}
-
-# Cream un data frame buit i anam copiant les llistes a la data frame
-IPC_INE_MAD_sub <- vctrs::data_frame(.size = 0)
-
-data <- as_tibble(my_function(raw_data, 1)) 
-IPC_INE_MAD_sub <- rbind(data, IPC_INE_MAD_sub)  %>% arrange(Data) %>% filter(Data > ymd(20020101))
-
-IPC_INE_MAD <- IPC_INE_MAD %>% 
-  cbind(IPC_INE_MAD_sub$Valor) %>%
-  rename(Subjacent = "IPC_INE_MAD_sub$Valor")
-
-toc()
-
-
-
-#################################################
-# INE 
-# Barcelona
-
-tic()
+###-----------------------------------------------------------------------------
+### Barcelona ###
 period <- days(today() - ymd(20000101))$day
 url <- str_c("https://servicios.ine.es/wstempus/js/ES/DATOS_METADATAOPERACION/IPC?g1=115:9&g2=3:74&g3=762:&p=1&nult=",period)
 res = GET(url)
@@ -150,36 +123,21 @@ categories <- fromJSON(content(res, "text", encoding = "UTF-8")) %>%
   as_tibble() %>%
   rename(Nombre = ".") %>%
   tidyr::separate(Nombre, into = c("Regio", "Categoria"), sep = "\\. ")
+raw_data <- fromJSON(rawToChar(res$content))$Data
 
-raw_data <- fromJSON(rawToChar(res$content))$Data 
-
-# Extreim les llistes
-my_function <- function(data, x) {
-  data_frame  <- data %>%
-    list.extract(x) %>%
-    mutate(Regio = categories[[1]][x],
-           Categoria = categories[[2]][x],
-           Data = as.POSIXct(Fecha/1000, origin="1970-01-01")) %>%
-    select(Categoria, Data, Valor, Regio)
-  data_frame
-}
-
-# Cream un data frame buit i anam copiant les llistes a la data frame
-IPC_INE_BAR <- vctrs::data_frame(.size = 0)
+# Create an empty data frame i copy the lists into the dataframe
+CPI_INE_BAR <- vctrs::data_frame(.size = 0)
 for (i in 1:13) {
-  data <- as_tibble(my_function(raw_data, i)) 
-  IPC_INE_BAR <- rbind(data, IPC_INE_BAR)
+  data <- as_tibble(extract_CPI(raw_data, i))
+  CPI_INE_BAR <- rbind(data, CPI_INE_BAR)
 }
+CPI_INE_BAR <- CPI_INE_BAR %>% arrange(Data) %>% filter(Data > ymd(20020101)) %>%
+  spread(key = "Categoria", value = "Valor") %>% mutate(Core = NA)
 
-IPC_INE_BAR <- IPC_INE_BAR %>% arrange(Data) %>% filter(Data > ymd(20020101)) %>% 
-  spread(key = "Categoria", value = "Valor") %>% mutate(Subjacent = NA)
-toc()
+# There is no information available aboout the core inflation of provincies
 
-
-#################################################
-# INE 
-# Catalunya
-
+###-----------------------------------------------------------------------------
+### Catalunya ###
 tic()
 period <- days(today() - ymd(20000101))$day
 url <- str_c("https://servicios.ine.es/wstempus/js/ES/DATOS_METADATAOPERACION/IPC?g1=70:9005&g2=3:74&g3=762:&p=1&nult=8104",period)
@@ -191,34 +149,18 @@ categories <- fromJSON(content(res, "text", encoding = "UTF-8")) %>%
   as_tibble() %>%
   rename(Nombre = ".") %>%
   tidyr::separate(Nombre, into = c("Regio", "Categoria"), sep = "\\. ")
+raw_data <- fromJSON(rawToChar(res$content))$Data
 
-raw_data <- fromJSON(rawToChar(res$content))$Data 
-
-# Extreim les llistes
-my_function <- function(data, x) {
-  data_frame  <- data %>%
-    list.extract(x) %>%
-    mutate(Regio = categories[[1]][x],
-           Categoria = categories[[2]][x],
-           Data = as.POSIXct(Fecha/1000, origin="1970-01-01")) %>%
-    select(Categoria, Data, Valor, Regio)
-  data_frame
-}
-
-# Cream un data frame buit i anam copiant les llistes a la data frame
-IPC_INE_CAT <- vctrs::data_frame(.size = 0)
+# Create an empty data frame i copy the lists into the dataframe
+CPI_INE_CAT <- vctrs::data_frame(.size = 0)
 for (i in 1:13) {
-  data <- as_tibble(my_function(raw_data, i)) 
-  IPC_INE_CAT <- rbind(data, IPC_INE_CAT)
+  data <- as_tibble(extract_CPI(raw_data, i))
+  CPI_INE_CAT <- rbind(data, CPI_INE_CAT)
 }
-
-IPC_INE_CAT <- IPC_INE_CAT %>% arrange(Data) %>% filter(Data > ymd(20020101)) %>% 
+CPI_INE_CAT <- CPI_INE_CAT %>% arrange(Data) %>% filter(Data > ymd(20020101)) %>%
   spread(key = "Categoria", value = "Valor")
 
-
-##########
-
-# IPC subjacent
+### Core CPI ###
 period <- days(today() - ymd(20000101))$day
 url <- str_c("https://servicios.ine.es/wstempus/js/ES/DATOS_METADATAOPERACION/IPC?g1=70:9005&g2=3:74&g3=269:12850&p=1&nult=",period)
 res =  GET(url)
@@ -230,35 +172,19 @@ categories <- fromJSON(content(res, "text", encoding = "UTF-8")) %>%
   as_tibble() %>%
   rename(Nombre = ".") %>%
   tidyr::separate(Nombre, into = c("Regio", "Categoria"), sep = "\\. ")
+raw_data <- fromJSON(rawToChar(res$content))$Data[1]
 
-raw_data <- fromJSON(rawToChar(res$content))$Data[1] 
+# Create an empty data frame i copy the lists into the dataframe
+CPI_INE_CAT_sub <- vctrs::data_frame(.size = 0)
+data <- as_tibble(extract_core_CPI(raw_data, 1))
+CPI_INE_CAT_sub <- rbind(data, CPI_INE_CAT_sub) %>%
+  arrange(Data) %>% filter(Data > ymd(20020101))
+CPI_INE_CAT <- CPI_INE_CAT %>%
+  cbind(CPI_INE_CAT_sub$Valor) %>%
+  rename(Core = "CPI_INE_CAT_sub$Valor")
 
-# Extreim les llistes
-my_function <- function(data, x) {
-  data_frame  <- data %>%
-    list.extract(x) %>%
-    mutate(Regio = categories[[1]][x],
-           Categoria = categories[[2]][x],
-           Data = as.POSIXct(Fecha/1000, origin="1970-01-01")) %>%
-    select(Data, Valor) 
-  data_frame
-}
-
-# Cream un data frame buit i anam copiant les llistes a la data frame
-IPC_INE_CAT_sub <- vctrs::data_frame(.size = 0)
-
-data <- as_tibble(my_function(raw_data, 1)) 
-IPC_INE_CAT_sub <- rbind(data, IPC_INE_CAT_sub)  %>% arrange(Data) %>% filter(Data > ymd(20020101))
-
-IPC_INE_CAT <- IPC_INE_CAT %>% 
-  cbind(IPC_INE_CAT_sub$Valor) %>%
-  rename(Subjacent = "IPC_INE_CAT_sub$Valor")
-toc()
-#################################################
-# INE 
-# Espanya
-
-tic()
+###-----------------------------------------------------------------------------
+### Spain ###
 period <- days(today() - ymd(20000101))$day
 url <- str_c("https://servicios.ine.es/wstempus/js/ES/DATOS_METADATAOPERACION/IPC?g1=349:16473&g2=3:74&g3=762:&p=1&nult=",period)
 res = GET(url)
@@ -272,35 +198,21 @@ categories <- fromJSON(content(res, "text", encoding = "UTF-8")) %>%
   filter(Impostos != "Impuestos constantes") %>%
   select(-c(Indicador,Impostos))
 
-# Identificam les files que contenen dades segons impostos constants
-posicions <- fromJSON(content(res, "text", encoding = "UTF-8")) %>% .$Nombre %>% str_detect("Impuestos")
+# Identify the rows that contains observations with constant taxes
+positions <- fromJSON(content(res, "text", encoding = "UTF-8")) %>%
+  .$Nombre %>% str_detect("Impuestos")
+raw_data <- fromJSON(content(res, "text", encoding = "UTF-8"))$Data[!positions]
 
-raw_data <- fromJSON(content(res, "text", encoding = "UTF-8"))$Data[!posicions]
-
-# Extreim les llistes
-my_function <- function(data, x) {
-  data_frame  <- data %>%
-    list.extract(x) %>%
-    mutate(Regio = categories[[1]][x],
-           Categoria = categories[[2]][x],
-           Data = as.POSIXct(Fecha/1000, origin="1970-01-01")) %>%
-    select(Categoria, Data, Valor, Regio)
-  data_frame
-}
-
-# Cream un data frame buit i anam copiant les llistes a la data frame
-IPC_INE_ESP <- vctrs::data_frame(.size = 0)
+# Create an empty data frame i copy the lists into the dataframe
+CPI_INE_ESP <- vctrs::data_frame(.size = 0)
 for (i in 1:13) {
-  data <- as_tibble(my_function(raw_data, i)) 
-  IPC_INE_ESP <- rbind(data, IPC_INE_ESP)
+  data <- as_tibble(extract_CPI(raw_data, i))
+  CPI_INE_ESP <- rbind(data, CPI_INE_ESP)
 }
-
-IPC_INE_ESP <- IPC_INE_ESP  %>% filter(Data > ymd(20020101)) %>% 
+CPI_INE_ESP <- CPI_INE_ESP  %>% filter(Data > ymd(20020101)) %>%
   spread(key = "Categoria", value = "Valor")
 
-##########
-
-# IPC subjacent
+### Core CPI ###
 period <- days(today() - ymd(20000101))$day
 url <- str_c("https://servicios.ine.es/wstempus/js/ES/DATOS_METADATAOPERACION/IPC?g1=349:16473&g2=3:74&g3=269:12850&p=1&nult=",period)
 res =  GET(url)
@@ -312,41 +224,23 @@ categories <- fromJSON(content(res, "text", encoding = "UTF-8")) %>%
   as_tibble() %>%
   rename(Nombre = ".") %>%
   tidyr::separate(Nombre, into = c("Regio", "Categoria"), sep = "\\. ")
-
-raw_data <- fromJSON(rawToChar(res$content))$Data[2] 
-
-# Extreim les llistes
-my_function <- function(data, x) {
-  data_frame  <- data %>%
-    list.extract(x) %>%
-    mutate(Regio = categories[[1]][x],
-           Categoria = categories[[2]][x],
-           Data = as.POSIXct(Fecha/1000, origin="1970-01-01")) %>%
-    select(Data, Valor) 
-  data_frame
-}
-
-# Cream un data frame buit i anam copiant les llistes a la data frame
-IPC_INE_ESP_sub <- vctrs::data_frame(.size = 0)
-
-data <- as_tibble(my_function(raw_data, 1)) 
-IPC_INE_ESP_sub <- rbind(data, IPC_INE_ESP_sub)  %>% arrange(Data) %>% filter(Data > ymd(20020101))
-
-IPC_INE_ESP <- IPC_INE_ESP %>% 
-  cbind(IPC_INE_ESP_sub$Valor) %>%
-  rename(Subjacent = "IPC_INE_ESP_sub$Valor")
+raw_data <- fromJSON(rawToChar(res$content))$Data[2]
+# Create an empty data frame i copy the lists into the dataframe
+CPI_INE_ESP_sub <- vctrs::data_frame(.size = 0)
+data <- as_tibble(extract_core_CPI(raw_data, 1))
+CPI_INE_ESP_sub <- rbind(data, CPI_INE_ESP_sub)  %>% arrange(Data) %>% filter(Data > ymd(20020101))
+CPI_INE_ESP <- CPI_INE_ESP %>%
+  cbind(CPI_INE_ESP_sub$Valor) %>%
+  rename(Core = "CPI_INE_ESP_sub$Valor")
 toc()
+###-----------------------------------------------------------------------------
+### Final table ###
 
-
-#################################################
-# Taula final
-
-IPC_INE <- IPC_INE_MAD %>% 
-  rbind(IPC_INE_CAT) %>% 
-  rbind(IPC_INE_BAR) %>% 
-  rbind(IPC_INE_ESP) %>%
+CPI_INE <- CPI_INE_MAD %>%
+  rbind(CPI_INE_CAT) %>%
+  rbind(CPI_INE_BAR) %>%
+  rbind(CPI_INE_ESP) %>%
   arrange(Data) %>%
   filter(!is.na(Data)) %>%
-  select(Data, Regio, `Índice general`, `Alimentos y bebidas no alcohólicas`:Subjacent)
-
-
+  select(Data, Regio, `Índice general`, `Alimentos y bebidas no alcohólicas`:Core)
+View(CPI_INE)
